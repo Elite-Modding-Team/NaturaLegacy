a995f9fdcdc870cc55299ad722812da98afe96f1
Me <135455255+IcarussOne@users.noreply.github.com>
2025-03-25 07:24:40 -0500

    Nether hoes break certain blocks faster
    Similar to what was done in 1.16.
    
    Affected blocks:
    -Cobwebs
    -Leaves
    -Plants
    -Sponge
    -Vines

--- src/main/java/com/progwml6/natura/entities/entity/passive/EntityImp.java
+++ src/main/java/com/progwml6/natura/entities/entity/passive/EntityImp.java
@@ -31,11 +31,13 @@ import net.minecraft.util.SoundEvent;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
-public class EntityImp extends EntityAnimal {
+public class EntityImp extends EntityAnimal
+{
     private static final DataParameter<Integer> SKIN_TYPE = EntityDataManager.<Integer>createKey(EntityImp.class, DataSerializers.VARINT);
 
-    public EntityImp(World par1World) {
-        super(par1World);
+    public EntityImp(World world)
+    {
+        super(world);
         this.setSize(0.75F, 1.45F);
         this.isImmuneToFire = true;
         this.tasks.addTask(0, new EntityAISwimming(this));
@@ -50,24 +52,28 @@ public class EntityImp extends EntityAnimal {
     }
 
     @Override
-    protected void applyEntityAttributes() {
+    protected void applyEntityAttributes()
+    {
         super.applyEntityAttributes();
         this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(20.0D);
         this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.25D);
     }
 
     @Override
-    protected void entityInit() {
+    protected void entityInit()
+    {
         super.entityInit();
         this.getDataManager().register(SKIN_TYPE, Integer.valueOf(Config.impVariants ? this.rand.nextInt(5) : 0));
     }
 
     @Override
-    protected void updateAITasks() {
+    protected void updateAITasks()
+    {
         super.updateAITasks();
     }
 
-    public int getSkin() {
+    public int getSkin()
+    {
         return this.dataManager.get(SKIN_TYPE).intValue();
     }
 
@@ -76,13 +82,15 @@ public class EntityImp extends EntityAnimal {
     }
 
     @Override
-    public void writeEntityToNBT(NBTTagCompound nbt) {
+    public void writeEntityToNBT(NBTTagCompound nbt)
+    {
         super.writeEntityToNBT(nbt);
         nbt.setInteger("Variant", getSkin());
     }
 
     @Override
-    public void readEntityFromNBT(NBTTagCompound nbt) {
+    public void readEntityFromNBT(NBTTagCompound nbt)
+    {
         super.readEntityFromNBT(nbt);
         setSkin(nbt.getInteger("Variant"));
     }
@@ -91,7 +99,8 @@ public class EntityImp extends EntityAnimal {
      * Returns the sound this mob makes while it's alive.
      */
     @Override
-    protected SoundEvent getAmbientSound() {
+    protected SoundEvent getAmbientSound()
+    {
         return SoundEvents.ENTITY_PIG_AMBIENT;
     }
 
@@ -99,7 +108,8 @@ public class EntityImp extends EntityAnimal {
      * Returns the sound this mob makes when it is hurt.
      */
     @Override
-    protected SoundEvent getHurtSound(DamageSource source) {
+    protected SoundEvent getHurtSound(DamageSource source)
+    {
         return SoundEvents.ENTITY_PIG_HURT;
     }
 
@@ -107,7 +117,8 @@ public class EntityImp extends EntityAnimal {
      * Returns the sound this mob makes on death.
      */
     @Override
-    protected SoundEvent getDeathSound() {
+    protected SoundEvent getDeathSound()
+    {
         return SoundEvents.ENTITY_PIG_DEATH;
     }
 
@@ -115,13 +126,15 @@ public class EntityImp extends EntityAnimal {
      * Plays step sound at given x, y, z for the entity
      */
     @Override
-    protected void playStepSound(BlockPos pos, Block blockIn) {
+    protected void playStepSound(BlockPos pos, Block block)
+    {
         this.playSound(SoundEvents.ENTITY_PIG_STEP, 0.15F, 1.0F);
     }
 
     @Override
     @Nullable
-    protected ResourceLocation getLootTable() {
+    protected ResourceLocation getLootTable()
+    {
         return NaturaEntities.IMP;
     }
 
@@ -130,22 +143,26 @@ public class EntityImp extends EntityAnimal {
      * the animal type)
      */
     @Override
-    public boolean isBreedingItem(ItemStack stack) {
+    public boolean isBreedingItem(ItemStack stack)
+    {
         return stack.getItem() == new ItemStack(NaturaNether.netherGlowshroom).getItem();
     }
 
     @Override
-    public EntityAgeable createChild(EntityAgeable par1EntityAgeable) {
+    public EntityAgeable createChild(EntityAgeable entity)
+    {
         return new EntityImp(this.world);
     }
 
     @Override
-    public boolean getCanSpawnHere() {
+    public boolean getCanSpawnHere()
+    {
     	return this.world.getBlockState((new BlockPos(this)).down()).canEntitySpawn(this) && (this.world.checkNoEntityCollision(this.getEntityBoundingBox()) && !this.world.containsAnyLiquid(this.getEntityBoundingBox()));
     }
 
     @Override
-    public float getEyeHeight() {
+    public float getEyeHeight()
+    {
         return this.height * 0.85F;
     }
 }
--- src/main/java/com/progwml6/natura/tools/item/tools/ItemNaturaHoe.java
+++ src/main/java/com/progwml6/natura/tools/item/tools/ItemNaturaHoe.java
@@ -1,15 +1,50 @@
 package com.progwml6.natura.tools.item.tools;
 
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemHoe;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 import com.progwml6.natura.Natura;
 
+// Unlike normal hoes, the Nether hoes will break certain blocks like leaves faster similar to what was done in 1.16
 public class ItemNaturaHoe extends ItemHoe
 {
-    public ItemNaturaHoe(ToolMaterial toolMaterialIn)
+    protected float efficiency;
+
+    public ItemNaturaHoe(ToolMaterial material)
     {
-        super(toolMaterialIn);
+        super(material);
 
         this.setCreativeTab(Natura.TAB);
+        this.efficiency = material.getEfficiency();
+    }
+
+    @Override
+    public boolean canHarvestBlock(IBlockState block)
+    {
+        Material material = block.getMaterial();
+        return material == Material.LEAVES || material == Material.VINE || material == Material.PLANTS || material == Material.WEB
+                || material == Material.SPONGE;
+    }
+
+    @Override
+    public float getDestroySpeed(ItemStack stack, IBlockState state)
+    {
+        return canHarvestBlock(state) ? this.efficiency : super.getDestroySpeed(stack, state);
+    }
+
+    @Override
+    public boolean onBlockDestroyed(ItemStack stack, World worldIn, IBlockState state, BlockPos pos, EntityLivingBase entityLiving)
+    {
+        if (!worldIn.isRemote && (double) state.getBlockHardness(worldIn, pos) != 0.0D)
+        {
+            stack.damageItem(1, entityLiving);
+        }
+
+        return true;
     }
 }
