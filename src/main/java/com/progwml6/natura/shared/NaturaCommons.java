90cf980b0aac8a0518c6051445fcecb338eeb4bc
IcarussOne <135455255+IcarussOne@users.noreply.github.com>
2026-05-06 06:21:59 -0500

    Replace poison with weakness for edibles

--- src/main/java/com/progwml6/natura/shared/NaturaCommons.java
+++ src/main/java/com/progwml6/natura/shared/NaturaCommons.java
@@ -178,12 +178,12 @@ public class NaturaCommons extends NaturaPulse
 
         if (isNetherLoaded())
         {
-            blightberry = edibles.addFood(6, 1, 0.4F, 16, "blightberry", false, new PotionEffect(MobEffects.REGENERATION, 8 * 20, 0), new PotionEffect(MobEffects.POISON, 5 * 20, 0), new PotionEffect(MobEffects.WITHER, 5 * 20, 0));
+            blightberry = edibles.addFood(6, 1, 0.4F, 16, "blightberry", false, new PotionEffect(MobEffects.REGENERATION, 8 * 20, 0), new PotionEffect(MobEffects.WEAKNESS, 5 * 20, 0), new PotionEffect(MobEffects.WITHER, 5 * 20, 0));
             duskberry = edibles.addFood(7, 1, 0.4F, 16, "duskberry", false, new PotionEffect(MobEffects.NIGHT_VISION, 15 * 20, 0), new PotionEffect(MobEffects.BLINDNESS, 3 * 20, 0));
             skyberry = edibles.addFood(8, 1, 0.4F, 16, "skyberry", false, new PotionEffect(MobEffects.JUMP_BOOST, 8 * 20, 0), new PotionEffect(MobEffects.SLOWNESS, 3 * 20, 0));
             stingberry = edibles.addFood(9, 1, 0.4F, 16, "stingberry", false, new PotionEffect(MobEffects.STRENGTH, 10 * 20, 0), new PotionEffect(MobEffects.MINING_FATIGUE, 10 * 20, 0));
 
-            potashApple = edibles.addFood(10, 4, 0.4F, 32, "potashapple", false, new PotionEffect(MobEffects.POISON, 2 * 25, 0));
+            potashApple = edibles.addFood(10, 4, 0.4F, 32, "potashapple", false, new PotionEffect(MobEffects.WEAKNESS, 2 * 20, 0));
         }
 
         cactusJuice = edibles.addFood(11, 1, 0.1f, 12, "cactusjuice", false);
