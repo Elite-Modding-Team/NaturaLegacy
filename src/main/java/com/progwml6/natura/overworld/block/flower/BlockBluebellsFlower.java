d0e3c571eab89f0ddac56cf82cafa535d82d3b58
IcarussOne <135455255+IcarussOne@users.noreply.github.com>
2026-05-06 21:20:00 -0500

    Bluebells now act like other flowers

--- src/main/java/com/progwml6/natura/overworld/block/flower/BlockBluebellsFlower.java
+++ src/main/java/com/progwml6/natura/overworld/block/flower/BlockBluebellsFlower.java
@@ -1,8 +1,18 @@
 package com.progwml6.natura.overworld.block.flower;
 
 import com.progwml6.natura.Natura;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockBush;
 import net.minecraft.block.SoundType;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraftforge.common.EnumPlantType;
+
+import javax.annotation.Nonnull;
 
 public class BlockBluebellsFlower extends BlockBush
 {
@@ -11,5 +21,38 @@ public class BlockBluebellsFlower extends BlockBush
         super();
         this.setSoundType(SoundType.PLANT);
         this.setCreativeTab(Natura.TAB);
+        this.setHardness(0.0F);
+    }
+
+    @Override
+    public int getFlammability(@Nonnull IBlockAccess world, @Nonnull BlockPos pos, @Nonnull EnumFacing face)
+    {
+        return Blocks.RED_FLOWER.getFlammability(world, pos, face);
+    }
+
+    @Override
+    public int getFireSpreadSpeed(@Nonnull IBlockAccess world, @Nonnull BlockPos pos, @Nonnull EnumFacing face)
+    {
+        return Blocks.RED_FLOWER.getFireSpreadSpeed(world, pos, face);
+    }
+
+    @Override
+    public @Nonnull Block.EnumOffsetType getOffsetType()
+    {
+        return Block.EnumOffsetType.XZ;
+    }
+
+    @SuppressWarnings("deprecation")
+    @Nonnull
+    @Override
+    public AxisAlignedBB getBoundingBox(IBlockState state, @Nonnull IBlockAccess world, @Nonnull BlockPos pos)
+    {
+        return BUSH_AABB.offset(state.getOffset(world, pos));
+    }
+
+    @Override
+    public @Nonnull EnumPlantType getPlantType(@Nonnull IBlockAccess world, @Nonnull BlockPos pos)
+    {
+        return EnumPlantType.Plains;
     }
 }
